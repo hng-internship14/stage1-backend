b21cfa2a0b0208da0de22934ae434e80d40d6e5f
hng-internship14 <sheidabdulahad0@gmail.com>
2026-04-18 17:58:34 -0700

    profile routes update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/profiles", profileRoutes);
+app.use("/api/profiles", profileRoutes);
 
 // Health check
 app.get("/", (req, res) => {
